d93c2a31d644b069bea9c9c40dc966d95bd66eb6
paulohenrique <paulohenrique300305@gmail.com>
2024-05-21 10:28:42 -0300

    :lipstick: feat: metodo remover client

--- student/src/main/java/com/fatec/student/resources/StudentController.java
+++ student/src/main/java/com/fatec/student/resources/StudentController.java
@@ -3,6 +3,7 @@ package com.fatec.student.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,9 @@ public class StudentController {
         return studentService.getStudentById(id);
     }
     
-
+    @DeleteMapping("{id}")
+    public void deleteStudentById(@PathVariable int id) {
+        this.studentService.deleteStudentById(id);
+    }
 
 }
