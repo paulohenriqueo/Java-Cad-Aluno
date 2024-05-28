b3082259c4d94095fae00f70305f0649d3434607
paulohenrique <paulohenrique300305@gmail.com>
2024-05-28 11:14:33 -0300

    :sparkles: feat: tratamento de excecoes

---
+++ student/src/main/java/com/fatec/student/resources/exceptions/ResourceExceptionHandle.java
@@ -0,0 +1,32 @@
+package com.fatec.student.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandle {
+    
+
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotfoundException(
+        EntityNotFoundException e,
+        HttpServletRequest request
+    ){
+        StandardError error = new StandardError();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Resource Not Found");
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(Instant.now());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
