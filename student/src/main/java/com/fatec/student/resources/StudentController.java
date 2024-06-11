cc8d424d38616a101e8a8537d38d6523ceaf29b3
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-11 10:57:05 -0300

    :sparkles: feat: @CrossOrigin

--- student/src/main/java/com/fatec/student/resources/StudentController.java
+++ student/src/main/java/com/fatec/student/resources/StudentController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("students")
 public class StudentController {
     
